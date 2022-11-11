df05adf9a2d956e42e8e8e4ae02f4a6c1e74ff22
Samer A <thedev.samer@gmail.com>
2022-11-11 16:33:59 +0200

    finished DOM manipulation on startup

---
+++ public/uiStuff.js
@@ -0,0 +1,26 @@
+let windowWidth = $(window).width();
+let windowHeight = $(window).height();
+let player = {};
+
+let canvas = document.querySelector("#the-canvas");
+let context = canvas.getContext("2d");
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+
+$(window).load(() => {
+  $("#loginModal").modal("show");
+});
+
+$(".name-form").submit((event) => {
+  event.preventDefault();
+  player.name = document.querySelector("#name-input").value.trim();
+  $("#loginModal").modal("hide");
+  $("#spawnModal").modal("show");
+  document.querySelector(".player-name").innerHTML = player.name;
+});
+
+$(".start-game").click((event) => {
+  $(".modal").modal("hide");
+  $(".hiddenOnStart").removeAttr("hidden");
+  init();
+});
